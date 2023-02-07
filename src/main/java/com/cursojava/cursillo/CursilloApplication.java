d312697046db0bfe340bfbe11e95a7d72a53b244
Matias <alumno.532989@ies-azarquiel.es>
2023-02-07 14:06:13 +0100

    Commit inicial (min.28)

---
+++ src/main/java/com/cursojava/cursillo/CursilloApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.cursillo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursilloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursilloApplication.class, args);
+	}
+
+}
